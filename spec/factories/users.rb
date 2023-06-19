4d1b3bd24b0a4b82857e4c7f513bd1345c2929f2
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-19 20:00:51 -0300

    generate user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    password_digest { "MyString" }
+    token { "MyString" }
+    auth_token { "MyString" }
+  end
+end
