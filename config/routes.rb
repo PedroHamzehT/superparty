9cf48a02ead4bf559667e762d31243e5c547c17b
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-22 16:35:32 -0300

    feat: adding magic link and create auth token routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :sessions, only: %i[create destroy]
-      resources :users, only: %i[create]
+      resources :sessions, only: %i[create destroy] do
+        collection do
+          post :magic_link
+        end
+      end
+      resources :users, only: %i[create] do
+        collection do
+          post :create_auth_token
+        end
+      end
     end
   end
 end
