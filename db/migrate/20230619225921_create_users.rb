15625d4bc81998c8b1d91df3abeab14aaf5929ff
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-22 17:41:18 -0300

    fix: adjust users table constraints

--- db/migrate/20230619225921_create_users.rb
+++ db/migrate/20230619225921_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false, unique: true
       t.string :first_name
       t.string :last_name
       t.string :password_digest
-      t.string :auth_token
+      t.string :auth_token, unique: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_003831) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest"
     t.string "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "auth_token_confirmed_at", null: false
+    t.datetime "auth_token_confirmed_at"
   end
 
 end
