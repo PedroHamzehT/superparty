9ab8c5997f8533913f92662d2329b27ee2fa2181
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-22 17:43:55 -0300

    chore: update magic link url temporarily

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@
 class UserMailer < ApplicationMailer
   def magic_link_email
     user = params[:user]
-    @magic_link = "http://localhost:3000/sessions/magic_link?auth_token=#{user.auth_token}"
+    @magic_link = "http://localhost:3000/api/v1/sessions/magic_link?auth_token=#{user.auth_token}"
     mail(to: user.email, subject: 'Log in Superparty with your magic link')
   end
 end
