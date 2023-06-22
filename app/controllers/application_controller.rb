cf64b7ae79328d5c0a5ba2db671088602b17a8fa
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-22 18:23:37 -0300

    feat: adding a default error response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def error_response(result: @result, status: :bad_request)
+    render json: { success: false, errors: result.errors }, status:
+  end
 end
