893290c82503cd3061a5cc85df233462a19d8370
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-23 22:56:36 -0300

    refact: improve base service to handle argument error when calling #call

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -5,30 +5,44 @@ class BaseService
 
   STANDARD_PUBLIC_METHODS = %i[errors call].freeze
 
-  def initialize
-    @errors = []
+  class << self
+    def call(arguments = {})
+      arguments = format_aguments(arguments)
+
+      service = new
+      service.call(**arguments)
+
+      service_response(service)
+    rescue ArgumentError => e
+      missing_arguments = e.message.split(' ')[2..].join(' ').gsub(':', '')
+      service.add_error("The following required arguments are missing: #{missing_arguments}")
+
+      service_response(service)
+    end
+
+    private
+
+    def service_response(service)
+      response_attributes = (service.public_methods(false) - STANDARD_PUBLIC_METHODS).reduce({}) do |response, attr|
+        response.merge({ attr => service.public_send(attr) })
+      end
+
+      OpenStruct.new({ success: service.errors.empty?, errors: service.errors }.merge(response_attributes))
+    end
+
+    def format_aguments(arguments)
+      arguments = arguments.to_h if arguments.is_a?(ActionController::Parameters)
+      arguments.deep_symbolize_keys
+    end
   end
 
-  def self.call(arguments = {})
-    service = new
-    service.call(**arguments)
-
-    service_response(service)
+  def initialize
+    @errors = []
   end
 
   def call
     NotImplementedError
   end
-
-  private_class_method def self.service_response(service)
-    response_attributes = (service.public_methods(false) - STANDARD_PUBLIC_METHODS).reduce({}) do |response, attr|
-      response.merge({ attr => service.public_send(attr) })
-    end
-
-    OpenStruct.new({ success: service.errors.empty?, errors: service.errors }.merge(response_attributes))
-  end
-
-  private
 
   def add_error(error)
     @errors << error
