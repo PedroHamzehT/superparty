ed51facf43f6ea927b722193517c71cc2a03ccf1
PedroHamzehT <pedrohamzeh@gmail.com>
2023-06-28 00:00:59 -0300

    feat: remove success value from error responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@
 
 class ApplicationController < ActionController::API
   def error_response(result: @result, status: :bad_request)
-    render json: { success: false, error: result.error }, status:
+    render json: { error: result.error }, status:
   end
 
   def object_not_found_error(object)
-    render json: { success: false, error: "#{object} not found " }, status: :bad_request
+    render json: { error: "#{object} not found " }, status: :bad_request
   end
 end
