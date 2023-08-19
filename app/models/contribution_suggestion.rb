a033c6903f4d6979299d11ea3b6ea4801b33442c
PedroHamzehT <pedrohamzeh@gmail.com>
2023-08-19 17:37:33 -0300

    refact: quick refactoring and improvements

--- app/models/contribution_suggestion.rb
+++ app/models/contribution_suggestion.rb
@@ -8,17 +8,21 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  contribution_id :bigint           not null
+#  user_id         :bigint           not null
 #
 # Indexes
 #
 #  index_contribution_suggestions_on_contribution_id  (contribution_id)
+#  index_contribution_suggestions_on_user_id          (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (contribution_id => contributions.id)
+#  fk_rails_...  (user_id => users.id)
 #
 class ContributionSuggestion < ApplicationRecord
   validates :name, presence: true
 
+  belongs_to :user
   belongs_to :contribution
 end
